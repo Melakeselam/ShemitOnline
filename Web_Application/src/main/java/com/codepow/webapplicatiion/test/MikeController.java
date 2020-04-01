308659bdcd54c9782ecc91e9c0c9d039595361d3
MichealSebre <msebre@gmail.com>
2020-04-01 16:52:11 -0400

    Mike
    Mike commit

---
+++ Web_Application/src/main/java/com/codepow/webapplicatiion/test/MikeController.java
@@ -0,0 +1,16 @@
+package com.codepow.webapplicatiion.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MikeController {
+	
+	@RequestMapping("/mike")
+	public String getMikeName()
+	{
+		
+		return "Welcome Mike"; 
+	}
+
+}
