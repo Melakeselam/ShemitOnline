d86bb6a020dfe2883c4db2bfca8b170e09017ae5
Melakeselam Moges <mmengistu@miu.edu>
2020-04-01 08:19:39 -0500

    the project skeleton

---
+++ Web_Application/src/test/java/com/codepow/webapplicatiion/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepow.webapplicatiion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
