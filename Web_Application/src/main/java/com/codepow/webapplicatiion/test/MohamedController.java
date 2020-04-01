8e776c4adb11242a5da79c7d5b65d089e5c1c6dc
Mohamed Soliman <idealtch@gmail.com>
2020-04-01 15:53:49 -0500

    Adding my Name
    Adding my nick name and my full name

---
+++ Web_Application/src/main/java/com/codepow/webapplicatiion/test/MohamedController.java
@@ -0,0 +1,14 @@
+package com.codepow.webapplicatiion.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MohamedController {
+	
+  @RequestMapping("/mhussein")
+  public String getMohamed() {
+	  String fullName="Mohamed Hussein Abdel Halim Mohamed";
+	  return fullName; 
+  }
+}
