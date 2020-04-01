09e3c5f62fe6c3d5b0c653e67af2db87567f5636
DerejeKenno <58951308+DerejeKenno@users.noreply.github.com>
2020-04-01 16:41:40 -0500

    dereje commit

---
+++ Web_Application/src/main/java/com/codepow/webapplicatiionTest/DerejeController.java
@@ -0,0 +1,14 @@
+package com.codepow.webapplicatiionTest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DerejeController {
+	@GetMapping("/dereje")
+	public String userAccount() {
+		return "Dereje Kenno";
+	}
+
+}
--- Web_Application/src/main/java/com/codepow/webapplicatiionTest/MelakeselamController.java
+++ Web_Application/src/main/java/com/codepow/webapplicatiionTest/MelakeselamController.java
@@ -1,4 +1,4 @@
-package com.codepow.webapplicatiion.test;
+package com.codepow.webapplicatiionTest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
